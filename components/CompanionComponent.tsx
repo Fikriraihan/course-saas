8ad66aa216b11003c6efd4b7789e8936e966d56e
Fikri Raihan <fikri.raihan4@gmail.com>
2025-06-03 19:34:29 +0700

    feat: Implement user profile

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -6,6 +6,7 @@ import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import soundwaves from "@/constants/soundwaves.json";
+import { addToSessionHistory } from "@/lib/actions/companion.actions";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -14,7 +15,7 @@ enum CallStatus {
   FINISHED = "FINISHED",
 }
 const CompanionComponent = ({
-  //   companionId,
+  companionId,
   subject,
   topic,
   name,
@@ -42,7 +43,10 @@ const CompanionComponent = ({
 
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
-    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+      addToSessionHistory(companionId);
+    };
     const onMessage = (message: Message) => {
       if (message.type === "transcript" && message.transcriptType === "final") {
         const newMessage = { role: message.role, content: message.transcript };
